8e2033d36fb199cb059a259070a3220f699962dc
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2025-04-11 19:38:59 +0400

    Convert Utils class to 'object' instead of 'class' with 'companion object'

--- libp2p/src/main/kotlin/io/libp2p/etc/util/MultiaddrUtils.kt
+++ libp2p/src/main/kotlin/io/libp2p/etc/util/MultiaddrUtils.kt
@@ -5,26 +5,23 @@ import io.libp2p.core.multiformats.Multiaddr
 import io.libp2p.core.multiformats.Protocol
 import java.net.*
 
-class MultiaddrUtils {
+object MultiaddrUtils {
 
-    companion object {
-
-        fun inetAddressToIpMultiaddr(addr: InetAddress): Multiaddr {
-            val proto = when (addr) {
-                is Inet4Address -> Protocol.IP4
-                is Inet6Address -> Protocol.IP6
-                else -> throw InternalErrorException("Unknown address type $addr")
-            }
-            return Multiaddr.empty()
-                .withComponent(proto, addr.hostAddress)
+    fun inetAddressToIpMultiaddr(addr: InetAddress): Multiaddr {
+        val proto = when (addr) {
+            is Inet4Address -> Protocol.IP4
+            is Inet6Address -> Protocol.IP6
+            else -> throw InternalErrorException("Unknown address type $addr")
         }
-
-        fun inetSocketAddressToTcpMultiaddr(addr: InetSocketAddress): Multiaddr =
-            inetAddressToIpMultiaddr(addr.address)
-                .withComponent(Protocol.TCP, addr.port.toString())
-
-        fun inetSocketAddressToUdpMultiaddr(addr: InetSocketAddress): Multiaddr =
-            inetAddressToIpMultiaddr(addr.address)
-                .withComponent(Protocol.UDP, addr.port.toString())
+        return Multiaddr.empty()
+            .withComponent(proto, addr.hostAddress)
     }
+
+    fun inetSocketAddressToTcpMultiaddr(addr: InetSocketAddress): Multiaddr =
+        inetAddressToIpMultiaddr(addr.address)
+            .withComponent(Protocol.TCP, addr.port.toString())
+
+    fun inetSocketAddressToUdpMultiaddr(addr: InetSocketAddress): Multiaddr =
+        inetAddressToIpMultiaddr(addr.address)
+            .withComponent(Protocol.UDP, addr.port.toString())
 }
