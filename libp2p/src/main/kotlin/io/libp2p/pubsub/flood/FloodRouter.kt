03264016ba23b97f149ed0a1137ba6f9a19ca97d
Lucas Saldanha <lucas.saldanha@consensys.net>
2026-02-13 11:30:42 +1300

    Add support for Extension Control Messages (gossipsub v1.3) 1/2 (#438)

--- libp2p/src/main/kotlin/io/libp2p/pubsub/flood/FloodRouter.kt
+++ libp2p/src/main/kotlin/io/libp2p/pubsub/flood/FloodRouter.kt
@@ -36,6 +36,10 @@ class FloodRouter(executor: ScheduledExecutorService = Executors.newSingleThread
         // NOP
     }
 
+    override fun processExtensions(msg: Rpc.RPC, receivedFrom: PeerHandler) {
+        // NOP
+    }
+
     private fun broadcast(msg: PubsubMessage, receivedFrom: PeerHandler?): CompletableFuture<Unit> {
         val sentFutures = peersForBroadcast(msg)
             .filter { it != receivedFrom }
