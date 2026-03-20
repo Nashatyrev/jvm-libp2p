d971e651f34f1d232f388907a4e6b8c8c6fd03b4
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2026-03-20 17:38:39 +0300

    Turn off Werror

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ configure(
         kotlinOptions.jvmTarget = "11"
         kotlinOptions {
             languageVersion = "1.6"
-            allWarningsAsErrors = true
+            allWarningsAsErrors = false
             freeCompilerArgs = listOf("-Xjvm-default=all")
         }
     }
