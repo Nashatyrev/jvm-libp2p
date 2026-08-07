ec459fef7eaab6fc5d09b0f31c7dd5bd48e21ea0
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2026-08-07 15:24:01 +0400

    AggregateControllable2 new draft handmade implementation

--- tools/quic-simulator/src/main/kotlin/io/libp2p/quicsim/core/schedule/AggregateControllable2.kt
+++ tools/quic-simulator/src/main/kotlin/io/libp2p/quicsim/core/schedule/AggregateControllable2.kt
@@ -1,21 +1,83 @@
 package io.libp2p.quicsim.core.schedule
 
 import io.libp2p.quicsim.core.NotifyingPacketEmitter
-import jdk.nashorn.internal.runtime.BitVector
+import java.util.SortedMap
+import java.util.TreeMap
+import kotlin.math.abs
 import kotlin.time.Duration
 
-open class AggregateControllable2(
-    private val controllables: List<Controllable>
-) : Controllable {
+open class AggregateControllable2<TControllable>(
+    private val controllables: List<TControllable>
+) : Controllable where TControllable : Controllable, TControllable : NotifyingPacketEmitter<*> {
 
+    private inner class RouteController(
+        val controllable: TControllable,
+    ) {
+        var curTime: Duration = Duration.ZERO
+        var nextTaskAbsolute: Duration? = null
+
+        init {
+            controllable.addPacketAddedListener { onTaskAdded() }
+            updateNextTaskTime()
+        }
+
+        fun onTaskAdded() {
+            if (nextTaskAbsolute == null) {
+                updateNextTaskTime()
+            }
+        }
+
+        private fun updateNextTaskTime() {
+            nextTaskAbsolute = controllable.nextTaskDuration()?.let { it + curTime }
+            if (nextTaskAbsolute != null) {
+                routeActivated(this)
+            }
+        }
+
+        fun advanceTillAbsolute(absoluteTime: Duration) {
+            val relativeAdvance = absoluteTime - curTime
+            controllable.advance(relativeAdvance)
+            curTime = absoluteTime
+        }
+
+        fun executePendingAndUpdateNextTaskTime() {
+            controllable.executePending()
+            updateNextTaskTime()
+        }
+    }
+
+    private var currentAbsoluteTime: Duration = Duration.ZERO
+    private val sortedRoutes: SortedMap<Duration, MutableList<RouteController>> = TreeMap()
+    private val allRoutes =
+        controllables.mapIndexed { index, controllable ->
+            RouteController(controllable)
+        }
+
+    // invoked when controllable having no tasks before got a task
+    // !!! Can be called from another thread
+    @Synchronized
+    private fun routeActivated(route: RouteController) {
+        sortedRoutes.computeIfAbsent(route.nextTaskAbsolute) { mutableListOf() } += route
+    }
+
+    @Synchronized
     override fun advance(advanceDuration: Duration) {
-        TODO()
+        val newTime = currentAbsoluteTime + advanceDuration
+        val nowRoutes = sortedRoutes.remove(newTime) ?: emptyList()
+        nowRoutes.forEach { route ->
+            route.advanceTillAbsolute(newTime)
+            route.executePendingAndUpdateNextTaskTime()
+        }
     }
 
+    @Synchronized
     override fun executePending() {
-        TODO()
+        // do nothing
     }
 
 
-    override fun nextTaskDuration(): Duration? = TODO()
+    @Synchronized
+    override fun nextTaskDuration(): Duration? =
+        if (sortedRoutes.isEmpty()) null
+        else sortedRoutes.firstKey()
 }
